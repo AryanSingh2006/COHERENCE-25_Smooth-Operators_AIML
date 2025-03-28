ebf7fc37fa0d63e246d930a123b6d46ede12a208
Aryan Singh <aryan.2402863101@vcet.edu.in>
2025-03-29 00:43:22 +0530

    Delete backend directory

--- backend/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); 
-const router = express.Router();
-const upload = require('../config/multerConfig');
-const { uploadResume } = require('../controllers/uploadController');
-
-// Upload Route - Max 2 Files
-router.post('/', upload.array('resume', 2), uploadResume);
-
-module.exports = router;
