993addeb81bf44cdfe200aba67afcc428391f11c
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-04-18 16:05:29 +0530

    .

--- backend/routes/resumeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const multer = require("../config/multerConfig");
-const { extractResumeData } = require("../controllers/resumeController");
-
-const router = express.Router();
-
-router.post("/upload", multer.single("resume"), extractResumeData);
-
-module.exports = router;
--- backend/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../config/multerConfig");
-const { uploadAndExtractText } = require("../controllers/uploadController");
-
-// Upload PDF and extract text
-router.post("/upload", upload.single("resume"), uploadAndExtractText);
-
-module.exports = router;
